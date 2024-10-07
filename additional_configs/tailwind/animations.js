3f55c4d57f5df30547c5755361e51be5b4a0c037
Karthik Vignesh Sundar <s.karthik6789+github_prof@gmail.com>
2024-10-07 18:40:47 +0530

    added animation for accordion

--- additional_configs/tailwind/animations.js
+++ additional_configs/tailwind/animations.js
@@ -1,3 +1,4 @@
+import { transform } from "typescript";
 
 
 const Animations = {
@@ -21,11 +22,48 @@ const Animations = {
                 // transform: "translateY(-100px)"
                 top: "-80px"
             }
-        }
+        },
+        rotate90: {
+            from: {
+                transform: "rotate(0deg)"
+            },
+            to: {
+                transform: "rotate(90deg)"
+            }
+        },
+        unrotate90: {
+            from: {
+                transform: "rotate(90deg)"
+            },
+            to: {
+                transform: "rotate(0deg)"
+            }
+        },
+        incheight: {
+            from: {
+                height: "0px",
+            },
+            to: {
+                height: "var(--skills-box-height)",
+
+            }
+        },
+        decheight: {
+            from: {
+                height: "var(--skills-box-height)",
+            },
+            to: {
+                height: "0px",
+            }
+        },
     },
     animation: {
         slideOut: "slideOut 400ms ease-in-out",
-        slideIn: "slideIn 500ms ease-in-out"
+        slideIn: "slideIn 500ms ease-in-out",
+        rotate90: "rotate90 200ms ease-in",
+        unrotate90: "unrotate90 200ms ease-in",
+        incheight: "incheight var(--height-animation-rate) ease-in-out",
+        decheight: "decheight var(--height-animation-rate) ease-in-out",
     }
 };
 
