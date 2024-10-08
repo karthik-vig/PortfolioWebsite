2e695cd27ab0f12d5e85e170a2f56e411a6ec6be
Karthik Vignesh Sundar <s.karthik6789+github_prof@gmail.com>
2024-10-09 03:15:25 +0530

    the education course appear in an alternating pattern based on odd or even position in the list

--- src/Education.tsx
+++ src/Education.tsx
@@ -2,7 +2,8 @@ import {
     Flex,
     Container,
     Box,
-    Heading
+    Heading,
+    Text,
 } from '@radix-ui/themes';
 import Data from './assets/data/data';
 
@@ -15,21 +16,94 @@ interface educationalQualification {
     degreeMark: string;
 }
 
-function Course({children} : {children: educationalQualification}) {
-    console.log(children);
+type directionSet = "row" | "row-reverse" | "column" | "column-reverse" | undefined;
+
+function CourseInformation({
+    children,
+    direction,
+}: {
+    children: educationalQualification, 
+    direction: directionSet,
+}) {
+    const durationComp = (
+        <Flex
+            direction="column"
+            justify="center"
+        >
+            <Text>
+                {children.duration}
+            </Text>
+        </Flex>
+    );
+    let placeOnTop = false;
+    let placeOnBottom = true;
+    if (direction === "row-reverse") {
+        placeOnTop = true;
+        placeOnBottom = false;
+    }
+    console.log(placeOnTop, placeOnBottom)
     return (
-        <Box>
-            <Flex>
+        <Flex
+            direction="row"
+            gap="1"
+            width="45%"
+        >
+            { placeOnTop? durationComp: <></> }
+            <Flex
+                direction="column"
+                gap="1"
+                width="auto"
+                dir="ltr"
+            >
+                <Heading as="h4">
+                    {children.institute}
+                </Heading>
+                <Text>
+                    {children.degree + " - " + children.course}
+                </Text>
+                <Text>
+                    {children.location}
+                </Text>
+                <Text>
+                    {children.degreeMark}
+                </Text>
+            </Flex>
+            { placeOnBottom? durationComp: <></> }
+        </Flex>
+    );
+}
+
+
+function Course({
+    children, 
+    direction,
+} : {
+    children: educationalQualification, 
+    direction: directionSet
+}) {
+    return (
+        <Box
+            width="100%"
+        >
+            <Flex
+                direction={direction}
+                gap="1"
+            >   
+                <CourseInformation 
+                    direction={direction}
+                >
+                    {children}
+                </CourseInformation>
             </Flex>
         </Box>
     );
 }
 
 export default function Education() {
-
     return (
         <Container
             id="education"
+            width="100%"
         >
             <Heading
                 as="h3"
@@ -39,12 +113,22 @@ export default function Education() {
             >
                 🎓 Education
             </Heading>
-            <Flex>
+            <Flex
+                direction="column"
+                gap="5"
+                height="auto"
+                className="\
+                ml-10 \
+                "
+            >
                 {
                     Data.education.map((element: object, idx: number) => {
+                        let direction: directionSet = "row";
+                        if (idx % 2 !== 0) direction = "row-reverse";
                         return (
                             <Course
                                 key={idx}
+                                direction={direction}
                             >
                                 {element as educationalQualification}
                             </Course>
