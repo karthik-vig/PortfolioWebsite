fb3de027542ad6391964cf458cb3772875700482
Karthik Vignesh Sundar <s.karthik6789+github_prof@gmail.com>
2024-10-14 01:49:48 +0530

    added a custom font face; not working yet

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ export default {
   ],
   theme: {
     extend: {
-      ...Animations
+      ...Animations,
+      fontFamily: {
+        pacifico: ["Pacifico", "sans-serif"]
+      },
     },
   },
   plugins: [],
