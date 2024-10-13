435e8ed9b588adbc45aafef1227fa7c60e593d8e
Karthik Vignesh Sundar <s.karthik6789+github_prof@gmail.com>
2024-10-14 01:53:31 +0530

    added the contact section

--- src/About.tsx
+++ src/About.tsx
@@ -3,38 +3,168 @@ import {
     Container,
     Text,
     Heading,
-    Box,
+    Flex,
+    Link,
+    LinkProps,
 } from '@radix-ui/themes';
 
+interface ContactInfoTemplate {
+    name: string;
+    email: string;
+    address: string;
+    linkedin: string;
+    github: string;
+}
+
+function Seperator() {
+    return (
+        <div
+            className="\
+            xs:opacity-0 \
+            sm:opacity-1 \
+            md:opacity-1 \
+            lg:opacity-1 \
+            xl:opacity-1 \
+            xs:min-w-[100%] \
+            sm:min-w-[100%] \
+            md:min-w-1 \
+            lg:min-w-1 \
+            xl:min-w-1 \
+            xs:min-h-1 \
+            sm:min-h-1 \
+            md:min-h-5 \
+            lg:min-h-5 \
+            xl:min-h-5 \
+            bg-slate-200 \
+            border-1 \
+            rounded-lg \
+            "
+        >
+        </div>
+    );
+}
+
+function ContactInfo({
+    children,
+}:{
+    children: ContactInfoTemplate
+}) {
+    const linkRenderSetting: LinkProps = {
+        target: "_black",
+        weight: "regular",
+        size: {
+            xs: "3",
+            sm: "3",
+            md: "6",
+        },
+        color: "red",
+        className: ""
+    };
+    return (
+        <Flex
+            direction={{
+                xs: "column",
+                sm: "column",
+                md: "row",
+            }}
+            gap="2"
+            justify="center"
+            align="center"
+        >
+            <Heading>
+                ✉️
+            </Heading>
+            <Link
+                href={`mailto:${children.email}`}
+                target={linkRenderSetting.target}
+                weight={linkRenderSetting.weight}
+                size={linkRenderSetting.size}
+                color={linkRenderSetting.color}
+            >
+                {children.email}
+            </Link>
+            <Seperator />
+            <Link
+                href={children.github}
+                target={linkRenderSetting.target}
+                weight={linkRenderSetting.weight}
+                size={linkRenderSetting.size}
+                color={linkRenderSetting.color}
+            >
+                Github
+            </Link>
+            <Seperator />
+            <Link
+                href={children.linkedin}
+                target={linkRenderSetting.target}
+                weight={linkRenderSetting.weight}
+                size={linkRenderSetting.size}
+                color={linkRenderSetting.color}
+            >
+                Linkedin
+            </Link>
+        </Flex>
+    );
+}
+
 export default function About() {
+    const emptyContactInfo: ContactInfoTemplate = {
+        name: "",
+        email: "",
+        address: "",
+        linkedin: "",
+        github: "",
+    };
     return (
         <Container 
             id="about"
             mt="100px"
+            className="\
+            h-[100vh] \
+            "
         >
-            <Heading
-                mb="6"
-                align="center"
-            >
-                🎯 About Me 🎯
-            </Heading>
-            <Box
+            <Flex
+                direction="column"
+                gap="5"
                 className="\
-                mx-10 \
+                min-h-[80vh] \
                 "
             >
+                <Heading
+                    mb="10%"
+                    mt="10%"
+                    align="center"
+                    size="9"
+                    className="\
+                    font-pacifico \
+                    "
+                >
+                    🎯 About Me 🎯
+                    {/* <p
+                        className="font-pacifico"
+                    >
+                        Hello
+                    </p> */}
+                </Heading>
                 <Text
                     size={{
                         xs: "3",
                         sm: "3",
-                        md: "3",
-                        lg: "3",
-                        xl: "5",
+                        md: "6",
+                        lg: "7",
+                        xl: "7",
                     }}
+                    className="\
+                    mx-10 \
+                    text-justify \
+                    "
                 >
                     {("profile" in Data.about[0])? Data.about[0].profile as string: ""}
                 </Text>
-            </Box>
+                <ContactInfo>
+                    {("contactInfo" in Data.about[0])? Data.about[0].contactInfo as ContactInfoTemplate: emptyContactInfo}
+                </ContactInfo>
+            </Flex>
         </Container>
     );
 }
