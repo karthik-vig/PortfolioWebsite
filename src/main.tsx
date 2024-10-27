087aafab5cb9be07187f42c128c7f917bcf4ff4f
Karthik Vignesh Sundar <s.karthik6789+github_prof@gmail.com>
2024-10-27 12:23:25 +0530

    move the radix styles import from the main.tsx file to the main.css file and before the tailwind imports to ensure tailwindcss imports take precendence over the radix style css for the same specificity

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import Main from './App.tsx'
-import '@radix-ui/themes/styles.css'
 import './assets/stylesheets/main.css'
 
 createRoot(document.getElementById('root')!).render( <Main />,);
