943c3b7b3e94a224089daa24da712e049c186817
Karthik Vignesh Sundar <s.karthik6789+github_prof@gmail.com>
2024-10-27 13:40:39 +0530

    added a vite plugin for gzip compression and added build optimizations to vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import viteCompression from 'vite-plugin-compression2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    viteCompression()
+  ],
+  build: {
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    minify: true,
+  },
 })
